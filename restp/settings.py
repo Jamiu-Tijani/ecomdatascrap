be863895009d260de0fd976c348046213b2bb536
Jamiu-Tijani <jimniz01@gmail.com>
2020-10-13 18:19:12 +0100

    lll

--- restp/settings.py
+++ restp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zo295myf3gt4@4hs#qo=v#a5ij#rngy)fifi=d!p=4#d@e1j99'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
